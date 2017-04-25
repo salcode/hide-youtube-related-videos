6314371b9e9611999816b2691b3a4fa0b5030a56
Sal Ferrarello <sal@ironcodestudio.com>
2017-04-25 11:06:35 -0400

    Setup brain monkey and load plugin in tests
    See #1

---
+++ tests/includes/bootstrap.php
@@ -0,0 +1,3 @@
+<?php
+
+define( 'HYRV_PLUGIN_ROOT_DIR', dirname( __FILE__ ) . '/../../' );
--- tests/tests/markup-modification.php
+++ tests/tests/markup-modification.php
@@ -1,6 +1,24 @@
 <?php
 
+use Brain\Monkey;
+use Brain\Monkey\Functions;
+
+
 class Hide_YouTube_Relative_Vidoes_TestCase extends PHPUnit_Framework_TestCase {
+
+	protected function setUp() {
+		parent::setUp();
+		Monkey::setUpWP();
+		Functions::when('register_activation_hook')->justReturn(true);
+
+		// Load the plugin.
+		require constant( 'HYRV_PLUGIN_ROOT_DIR' ) . 'hide-youtube-related-videos.php';
+	}
+
+	protected function tearDown() {
+		Monkey::tearDownWP();
+		parent::tearDown();
+	}
 
 	function test_dummy() {
 		$a = true;
