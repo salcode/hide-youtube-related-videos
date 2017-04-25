eb715d8ba71e8cc9c7c92d9d41f1cf54b1dfe23c
Sal Ferrarello <sal@ironcodestudio.com>
2017-04-25 11:39:10 -0400

    Add test for video and playlist add rel=0
    Note: We are not using Brain Monkey at this time.
    
    Ultimately, we mocked the WP functions ourselves instead of using Brain
    Monkey because as soon as the plugin is loaded, the functions fired.
    Which caused a problem not being able to mock the functions early enough
    with Brain Monkey.
    
    I suspect there is a better solution to this problem, but I've not yet
    found it.
    
    At this point, Brain Monkey is unused and could be removed.
    
    See #1

--- tests/includes/bootstrap.php
+++ tests/includes/bootstrap.php
@@ -1,3 +1,14 @@
 <?php
 
 define( 'HYRV_PLUGIN_ROOT_DIR', dirname( __FILE__ ) . '/../../' );
+
+// Mock WordPress functions.
+function apply_filters( $filter_name, $val ) { return $val; }
+function add_action() { return true; }
+function add_filter() { return true; }
+function register_activation_hook() { return true; }
+
+// Load the plugin.
+if ( file_exists( HYRV_PLUGIN_ROOT_DIR . 'hide-youtube-related-videos.php' ) ) {
+	require_once  HYRV_PLUGIN_ROOT_DIR . 'hide-youtube-related-videos.php';
+}
--- tests/tests/markup-modification.php
+++ tests/tests/markup-modification.php
@@ -6,13 +6,19 @@ use Brain\Monkey\Functions;
 
 class Hide_YouTube_Relative_Vidoes_TestCase extends PHPUnit_Framework_TestCase {
 
+	// Example Video Details.
+	const ORIG_VIDEO_URL     = 'https://www.youtube.com/watch?v=cSj-etJL3nI';
+	const ORIG_VIDEO_EMBED   = '<iframe width="500" height="281" src="https://www.youtube.com/embed/cSj-etJL3nI?feature=oembed" frameborder="0" allowfullscreen></iframe>';
+	const NO_REL_VIDEO_EMBED = '<iframe width="500" height="281" src="https://www.youtube.com/embed/cSj-etJL3nI?wmode=transparent&amp;rel=0&amp;feature=oembed" frameborder="0" allowfullscreen></iframe>';
+
+	// Example Playlist Details.
+	const ORIG_PLAYLIST_URL     = 'https://www.youtube.com/playlist?list=PLwJS-G75vM7kFO-yUkyNphxSIdbi_1NKX';
+	const ORIG_PLAYLIST_EMBED   = '<iframe width="500" height="281" src="https://www.youtube.com/embed/videoseries?list=PLwJS-G75vM7kFO-yUkyNphxSIdbi_1NKX" frameborder="0" allowfullscreen></iframe>';
+	const NO_REL_PLAYLIST_EMBED = '<iframe width="500" height="281" src="https://www.youtube.com/embed/videoseries?wmode=transparent&amp;rel=0&amp;list=PLwJS-G75vM7kFO-yUkyNphxSIdbi_1NKX" frameborder="0" allowfullscreen></iframe>';
+
 	protected function setUp() {
 		parent::setUp();
 		Monkey::setUpWP();
-		Functions::when('register_activation_hook')->justReturn(true);
-
-		// Load the plugin.
-		require constant( 'HYRV_PLUGIN_ROOT_DIR' ) . 'hide-youtube-related-videos.php';
 	}
 
 	protected function tearDown() {
@@ -20,9 +26,22 @@ class Hide_YouTube_Relative_Vidoes_TestCase extends PHPUnit_Framework_TestCase {
 		parent::tearDown();
 	}
 
-	function test_dummy() {
-		$a = true;
-		$this->assertTrue( $a );
+	function test_oembed_video_add_no_rel() {
+		$result = hide_youtube_related_videos(
+			self::ORIG_VIDEO_EMBED,
+			self::ORIG_VIDEO_URL,
+			array()
+		);
+		$this->assertEquals( self::NO_REL_VIDEO_EMBED, $result );
+	}
+
+	function test_oembed_playlist_add_no_rel() {
+		$result = hide_youtube_related_videos(
+			self::ORIG_PLAYLIST_EMBED,
+			self::ORIG_PLAYLIST_URL,
+			array()
+		);
+		$this->assertEquals( self::NO_REL_PLAYLIST_EMBED, $result );
 	}
 
 }
